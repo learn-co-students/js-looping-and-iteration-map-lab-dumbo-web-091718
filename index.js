ba54f486302d486436ae8ee42ece3f7478b6ed1d
DonovanDwyer <dwyer.donovan@gmail.com>
2018-10-28 15:48:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file.
+function lowerCaseDrivers (array) {
+  return array.map(function (el) { return el.toLowerCase(); });
+}
+
+function nameToAttributes (array) {
+  return array.map(function (n) {
+    let first = n.split(" ")[0];
+    let last = n.split(" ")[1];
+
+    return { firstName: first, lastName: last};
+  });
+}
+
+function attributesToPhrase (array) {
+  return array.map(function (a) { return `${a.name} is from ${a.hometown}`} );
+}
